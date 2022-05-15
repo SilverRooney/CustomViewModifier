ac766212d17cb9ce28b3a70e9320499c16d5e5db
SilverRooney <silver.rooney.dev@gmail.com>
2022-05-15 12:10:40 -0500

    Initial Commit

---
+++ CustomViewModifier/CustomViewModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomViewModifierApp.swift
+//  CustomViewModifier
+//
+//  Created by Silver on 5/15/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomViewModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
